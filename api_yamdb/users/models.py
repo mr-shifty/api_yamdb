3fcaa0c5f35dc7d81eee5be7c6d9b772936f6895
Pavel Melnikov <room.apps@mail.ru>
2023-04-01 11:52:39 +0300

    Finalized model user.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -26,17 +26,6 @@ class User(AbstractUser):
         blank=False,
         null=False
     )
-    role = models.CharField(
-        'роль',
-        max_length=20,
-        choices=ROLE_CHOICES,
-        default=USER,
-        blank=True
-    )
-    bio = models.TextField(
-        'биография',
-        blank=True,
-    )
     first_name = models.CharField(
         'имя',
         max_length=150,
@@ -47,11 +36,44 @@ class User(AbstractUser):
         max_length=150,
         blank=True
     )
+    bio = models.TextField(
+        'биография',
+        blank=True,
+    )
+    role = models.CharField(
+        'роль',
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=USER,
+        blank=True
+    )
+    confirmation_code = models.CharField(
+        max_length=5, null=True,
+        verbose_name='Код подтверждения'
+    )
+
+    @property
+    def is_user(self):
+        return self.role == USER
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
 
     class Meta:
         ordering = ('id',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'],
+                name='uq_username_email'
+            ),
+        ]
 
     def __str__(self):
         return self.username
