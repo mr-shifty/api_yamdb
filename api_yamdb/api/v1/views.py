7bbeda9835174d0d99fcdb860fae52e7ec658217
eduard Nasyrov <bem6u.nasyrov@gmail.com>
2023-04-02 14:48:57 +0300

    urls and views

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets, mixins, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from reviews.models import Category, Genre, Review, Title
+from .filters import TitleFilter
+from .permissions import (IsAdminUserOrReadOnly)
+from .serializers import (CategorySerializer,
+                          GenreSerializer, TitleSerializer)
+
+
+class ListCreateDestroyViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    lookup_field = 'slug'
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+
+class CategoryViewSet(ListCreateDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+class GenreViewSet(ListCreateDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.annotate(
+        rating=Avg('reviews__score')).order_by('rating')
+    serializer_class = TitleSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+    pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TitleFilter
+
+    def perform_create(self, serializer):
+        category = get_object_or_404(
+            Category, slug=self.request.data.get('category')
+        )
+        genre = Genre.objects.filter(
+            slug__in=self.request.data.getlist('genre')
+        )
+        serializer.save(category=category, genre=genre)
+
+    def perform_update(self, serializer):
+        self.perform_create(serializer)
