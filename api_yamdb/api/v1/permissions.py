000a8d06caa16c0dd03ff726086afece793db66b
eduard Nasyrov <bem6u.nasyrov@gmail.com>
2023-04-02 18:54:09 +0300

    urls views serializers permissions

---
+++ api_yamdb/api/v1/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminUserOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin)
