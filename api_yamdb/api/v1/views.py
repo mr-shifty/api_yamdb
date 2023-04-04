63bd1136608b0231483adc4f0759090ec0439c66
Валерия Лаврикова <lavrikova219@yandex.ru>
2023-04-04 11:03:19 +0300

    Permissions review

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -6,7 +6,12 @@ from rest_framework.pagination import LimitOffsetPagination
 
 from reviews.models import Category, Genre, Title, Review
 from .filters import TitleFilter
-from .permissions import (IsAdminUserOrReadOnly)
+from .permissions import (
+    IsAdminUserOrReadOnly,
+    IsAuthorOrReadOnly,
+    IsModeratorOrReadOnly,
+    IsAuthenticatedOrAdmin
+)
 from .serializers import (
     CategorySerializer,
     GenreSerializer,
@@ -63,7 +68,7 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = [IsAdminUserOrReadOnly, ]
+    permission_classes = [IsAuthorOrReadOnly, ]
 
     def get_queryset(self):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
@@ -76,7 +81,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAdminUserOrReadOnly, ]
+    permission_classes = [IsAuthorOrReadOnly, ]
 
     def get_queryset(self):
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
