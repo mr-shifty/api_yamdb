e509ca437e30739e7dfe5f6758c5de2b767fafb4
Валерия Лаврикова <lavrikova219@yandex.ru>
2023-04-05 12:59:37 +0300

    Fixed_review_comment.

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -71,7 +71,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        return title.reviews.all()
+        return title.reviews.all().select_related('author')
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
@@ -84,7 +84,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
-        return review.comments.all()
+        return review.comments.all().select_related('author')
 
     def perform_create(self, serializer):
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
